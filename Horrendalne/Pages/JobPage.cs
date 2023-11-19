c2d267d1b1903310a5982ea904dd2706480ad819
Jan Tuziak <jan.tuziak@gmail.com>
2023-11-19 16:50:54 +0100

    add JobPage.cs

---
+++ Horrendalne/Pages/JobPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horrendalne.Pages
+{
+    public abstract class JobPage
+    {
+        protected string Url;
+
+        public JobPage(string url)
+        {
+            Url = url;
+        }
+    }
+}
